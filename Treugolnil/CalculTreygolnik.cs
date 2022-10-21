3f4f6d1033025d977592e72c86319e519b683a4f
НаумовСА <@211-02>
2022-10-21 12:25:43 +0300

    Добавьте файлы проекта.

---
+++ Treugolnil/CalculTreygolnik.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugolnil
+{
+    internal class CalculTreygolnik
+    {
+
+        public string checkTreug(int a, int b, int c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a)
+            {
+                return "Треугольника с такими сторонами не существует!!!";
+            }
+            if (a == b && a == c && b == c)
+            {
+                return "Треугольник равносторонний!";
+            }
+            if (((a * a) == (b * b) + (c * c)) || ((b * b) == ((a * a) + (c * c)) || ((c * c) == (b * b) + (a * a))))
+            {
+                return "Треугольник прямоугольный!";
+            }
+            if ((a == b && a != c) || (b == c && b != a) || (a == c && a != b))
+            {
+                return "Треугольник равнобедренный!";
+            }
+            return "Треугольник разносторонний!";
+        }
+    }
+}
---
+++ Treugolnil/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treugolnil
+{
+    internal class Program
+    {
+        public static int checkNumber(string numb)
+        {
+            try
+            {
+                int num = int.Parse(numb);
+                if (num != 0 && num > 0 && num < 1000)
+                {
+                    return num;
+                }
+                else
+                    Console.WriteLine("Число не подошло, и будет замененно на 0");
+                    return 0;
+
+            }
+            catch (FormatException) { return 0; }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника");
+            
+            Console.WriteLine("Первая сторона: ");
+            int wallOne = checkNumber(Console.ReadLine());
+            
+            Console.WriteLine("Вторая сторона: ");
+            int wallTwo = checkNumber(Console.ReadLine());
+
+            Console.WriteLine("Третья сторона: ");
+            int wallThree = checkNumber(Console.ReadLine());
+
+            CalculTreygolnik treygolnik = new CalculTreygolnik();
+            String result = treygolnik.checkTreug(wallOne, wallTwo, wallThree);
+            Console.WriteLine($"{result}");
+
+            Console.ReadKey();
+        }
+        
+    }
+}
